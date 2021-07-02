aa27cf739bb846dc5313edbf81b6d9ca5821af5d
brothersoo <moistybro@gmail.com>
2021-07-02 11:40:39 +0900

    ADD: entities

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -2,17 +2,19 @@ package jpabook.jpashop.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import jpabook.jpashop.entity.Member;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Rollback(value = false)
 class MemberRepositoryTest {
 
   @Autowired MemberRepository 회원저장소;
@@ -23,11 +25,11 @@ class MemberRepositoryTest {
   void memberSaveTest() throws Exception {
     String 회원이름 = "brothersoo";
     Member 회원 = new Member();
-    회원.setUsername(회원이름);
+    회원.setName(회원이름);
 
     Long 회원Id = 회원저장소.save(회원);
 
     assertThat(회원저장소.find(회원Id).getId()).isEqualTo(회원Id);
-    assertThat(회원저장소.find(회원Id).getUsername()).isEqualTo(회원이름);
+    assertThat(회원저장소.find(회원Id).getName()).isEqualTo(회원이름);
   }
 }
